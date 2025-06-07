af12456a45b2c7b6e8ea37c367fc635fd7c0c4dd
Tanuj26Rajput <tanuj26rajput@gmail.com>
2025-06-07 11:58:46 +0530

    preparing for deployment

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+import tensorflow.keras.models import load_model
+import tensorflow.keras.preprocessing.sequence as pad_sequences
+import numpy as np
+
+model = load_model("Spam-Email-Classifier.h5")
+with open("tokenizer.pickle", "rb") as f:
+    tokenizer = pickle.load(f)
+
+def predict(text):
+    seq = tokenizer.texts_to_sequnces([text])
+    padded = pad_sequences(seq, maxlen=100, padding="post", truncating="post")
+    prediction = model.predict(padded)[0][0]
+    return "Spam" if prediction < 0.5 else "Not Spam"
+
+st.set_page_config(page_title="Spam Email Classifier", page_icon="📧")
+st.title("📩 Spam Email Classifier")
+st.markdown("Enter the email content below to check is it's spam or not.")
+
+content = st.text_input("Email Content", height=200)
+
+if st.button("Classify"):
+    if content:
+        result = predict(content)
+        st.success(f"The email is classsified as: {result}")
+    else:
+        st.error("Please enter the email content to classify.")
